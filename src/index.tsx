f26e07975444df776e2fd9cf3d33780c5610b51d
mo khosravi <mohammadreza.khosravivala@nn.nl>
2024-05-21 20:11:07 +0200

    react app WORKS!!!@

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,14 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-	<BrowserRouter basename={process.env.PUBLIC_URL}>
+	<React.StrictMode>
 		<App />
-	</BrowserRouter>,
+	</React.StrictMode>,
 );
 
+// <BrowserRouter basename={process.env.PUBLIC_URL}>
+// 	<App />
+// </BrowserRouter>,
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
